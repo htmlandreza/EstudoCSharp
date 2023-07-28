673db5d2d89f28a0d8f61eb91ac7cb88a7b3b2b4
Andreza Moreira <htmlandreza@gmail.com>
2023-07-28 09:03:08 -0300

    Adicionando projeto complementar

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Nesse repositório foram feitos alguns cursos, projetos e guia de exemplos:
 ### 1. Fundamentos de C# - Balta.io
 
-* Projeto: MeuApp 
+* Projeto: SimplesApp 
   * Tipos de Conversão de flost pra int: Parse x Convert
---
+++ SimplesApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SimplesApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* Conversão - flost pra int */
+        // declarando variáveis
+        int inteiro = 10;
+        float real = 10.8f;
+
+        /* Conversão com Parse não funciona nesse caso
+        obs: parse sempre espera receber uma string */
+        //inteiro = int.Parse(real.ToString());
+
+        /* Conversão com Convert 
+        obs: no caso de float pra int, ele sempre irá arredondar o valor*/
+        inteiro = Convert.ToInt32(real);
+
+        Console.WriteLine("Conversão do tipo Convert de real pra inteiro: " + inteiro);
+    }
+}
