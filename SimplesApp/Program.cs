f485956d7b17dbbe5f287e756833266a1afb67b7
Andreza Moreira <htmlandreza@gmail.com>
2023-07-29 07:51:07 -0300

    Exemplo de uso de conversão com Convert

--- SimplesApp/Program.cs
+++ SimplesApp/Program.cs
@@ -16,7 +16,8 @@ class Program
         /* Conversão com Convert 
         obs: no caso de float pra int, ele sempre irá arredondar o valor*/
         inteiro = Convert.ToInt32(real);
-
         Console.WriteLine("Conversão do tipo Convert de real pra inteiro: " + inteiro);
+
+        Console.WriteLine("Conversão pra Bool, dentro do console" + Convert.ToBoolean(real));
     }
 }
