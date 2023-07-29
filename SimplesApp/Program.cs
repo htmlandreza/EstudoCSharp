d536f9bd02178bc734c9fd8f1a52345dcdeeb131
Andreza Moreira <htmlandreza@gmail.com>
2023-07-29 08:23:54 -0300

    Uso de operadores: aritméticos, atribuição e comparação

--- SimplesApp/Program.cs
+++ SimplesApp/Program.cs
@@ -17,7 +17,18 @@ class Program
         obs: no caso de float pra int, ele sempre irá arredondar o valor*/
         inteiro = Convert.ToInt32(real);
         Console.WriteLine("Conversão do tipo Convert de real pra inteiro: " + inteiro);
+        // usando Convert dentro do Console
+        Console.WriteLine("Conversão pra Bool, dentro do console: " + Convert.ToBoolean(real));
 
-        Console.WriteLine("Conversão pra Bool, dentro do console" + Convert.ToBoolean(real));
+
+        /* Operadores */
+        // Atrubuição
+        int x = 2;
+        x += 2; // 4
+        Console.WriteLine("x = " + x);
+
+        // Comparação
+        Console.WriteLine("Comparação: o valor de X é igual de 25?");
+        Console.WriteLine(x == 25); //Não é possivel colocar strings juntas nesse caso que contem comparação
     }
 }
